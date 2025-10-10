5b890003387c55c8cddc5e163ab4f3e12ea06d3d
rahulrs21 <rahul9449766@gmail.com>
2025-10-10 18:33:13 +0400

    updated resend2

--- app/services/pre-purchase-inspection/page.tsx
+++ app/services/pre-purchase-inspection/page.tsx
@@ -1,4 +1,4 @@
-import FormDataEmail from '@/components/FormDataEmail'
+ 
 import SubmitDataSheet from '@/components/SubmitDataSheet'
 import { Sub } from '@radix-ui/react-context-menu'
 import React from 'react'
@@ -7,9 +7,7 @@ import { Form } from 'react-hook-form'
 function PrePageInspection() {
   return (
     <div className='min-h-screen pt-24 bg-gray-800 dark:bg-gray-900 text-white flex flex-col items-center'>
-
-      <FormDataEmail />
-      
+ 
     <SubmitDataSheet />
 
     </div>
